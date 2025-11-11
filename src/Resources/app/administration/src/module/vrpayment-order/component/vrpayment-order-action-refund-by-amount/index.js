55bdb5c640ae7bb6ccf49360c1ff7e7aefdf2f10
andrewrowanwallee <andrew.rowan@wallee.com>
2025-11-11 13:52:29 +0100

    Release 7.1.5

--- src/Resources/app/administration/src/module/vrpayment-order/component/vrpayment-order-action-refund-by-amount/index.js
+++ src/Resources/app/administration/src/module/vrpayment-order/component/vrpayment-order-action-refund-by-amount/index.js
@@ -70,14 +70,17 @@ Component.register('vrpayment-order-action-refund-by-amount', {
 				});
 			}).catch((errorResponse) => {
 				try {
-					var errorTitle;
+					var errorTitle = errorResponse?.response?.data?.errors?.[0]?.title ?? this.$tc('vrpayment-order.refundAction.refundCreateError.errorTitle')
 					var errorMessage;
-					if (errorResponse.response.data == 'refundExceedsAmount') {
-						errorTitle = this.$tc('vrpayment-order.refundAction.refundExceedsTotalError.title');
-						errorMessage = this.$tc('vrpayment-order.refundAction.refundExceedsTotalError.messageRefundAmountExceedsAvailableBalance');
-					} else {
-						errorTitle = errorResponse.response.data.errors[0].title;
-						errorMessage = errorResponse.response.data.errors[0].detail;
+					switch(errorResponse.response.data) {
+						case 'refundAmountZero':
+							errorMessage = this.$tc('vrpayment-order.refundAction.refundCreateError.messageRefundAmountIsZero');
+						break;
+						case 'refundExceedsAmount':
+							errorMessage = this.$tc('vrpayment-order.refundAction.refundCreateError.messageRefundAmountExceedsAvailableBalance');
+						break;
+						default:
+							errorMessage = errorResponse.response.data.errors[0].detail;
 					}
 					this.createNotificationError({
 						title: errorTitle,
